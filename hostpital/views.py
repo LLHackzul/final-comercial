56de575e3ca9a306d2d6db36028362354b1975af
LLHackzul <alejandromkup24@gmail.com>
2021-11-28 17:23:05 -0600

    added list, edit and delete functions

--- hostpital/views.py
+++ hostpital/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Mantenimiento
 from .forms import MantenimientoForm
+
+
+def mantenimiento_list(request):
+    mantenimiento = Mantenimiento.objects.all()
+    return render(request, 'hostpital/mantenimiento_list.html', {'mantenimientos': mantenimiento})
 
 def new_mantenimiento(request):
     if request.method == "POST":
@@ -7,6 +13,23 @@ def new_mantenimiento(request):
         if form.is_valid():
             Mantenimiento = form.save(commit=False)
             Mantenimiento.save()
+            return redirect('mantenimiento_list')
     else:
         form= MantenimientoForm()
     return render(request, 'hostpital/new_mantenimiento.html',{'form': form})
+
+def mantenimiento_edit(request, pk):
+    mant = get_object_or_404(Mantenimiento, pk=pk)
+    if request.method == "POST":
+        form = MantenimientoForm(request.POST, instance=mant)
+        if form.is_valid():
+            mant = form.save(commit=False)
+            mant.save()
+            return redirect('mantenimiento_list')
+    else:
+        form = MantenimientoForm(instance=mant)
+    return render(request, 'hostpital/mantenimiento_edit.html', {'form': form})
+def mantenimiento_remove(request, pk):
+    mantenimiento = get_object_or_404(Mantenimiento, pk=pk)
+    mantenimiento.delete()
+    return redirect('mantenimiento_list')
