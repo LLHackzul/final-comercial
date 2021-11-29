ddb45d5c762aad342c7c24b8fc5e46c87ebec789
147leonel <ser.leo.147@gmail.com>
2021-11-28 20:40:04 -0600

    added CRUD doctores

--- hostpital/urls.py
+++ hostpital/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('doctor/', views.new_doctor, name='new_doctor'),
+    path('doctor/', views.doctor_list, name='doctor_list'),
+    path('agregar/doctor/', views.new_doctor, name='new_doctor'),
+    path('doctor/<pk>/edit/', views.doctor_edit, name='doctor_edit'),
+    path('doctor/<pk>/remove/', views.doctor_remove, name='doctor_remove'),
 ]
--- hostpital/views.py
+++ hostpital/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-
-from hostpital.models import Doctores
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Doctores
 from .forms import DoctoresForm
+
+
+def doctor_list(request):
+    doctor = Doctores.objects.all()
+    return render(request, 'hostpital/doctor_list.html', {'doctoress': doctor})
 
 def new_doctor(request):
     if request.method == "POST":
@@ -11,6 +14,23 @@ def new_doctor(request):
         if form.is_valid():
             Doctores = form.save(commit=False)
             Doctores.save()
+            return redirect('doctor_list')
     else:
         form= DoctoresForm()
     return render(request, 'hostpital/new_doctor.html',{'form': form})
+
+def doctor_edit(request, pk):
+    doc = get_object_or_404(Doctores, pk=pk)
+    if request.method == "POST":
+        form = DoctoresForm(request.POST, instance=doc)
+        if form.is_valid():
+            doc = form.save(commit=False)
+            doc.save()
+            return redirect('doctor_list')
+    else:
+        form = DoctoresForm(instance=doc)
+    return render(request, 'hostpital/doctor_edit.html', {'form': form})
+def doctor_remove(request, pk):
+    doctor = get_object_or_404(Doctores, pk=pk)
+    doctor.delete()
+    return redirect('doctor_list') 
