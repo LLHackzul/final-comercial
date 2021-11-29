0b3cc8a9c39b2de4b317d092d4ea6111b7b61d73
DerickCoy <derickcoy@mesoamericana.edu.gt>
2021-11-28 20:48:14 -0600

    added CRRUD PACIENTES

--- hostpital/urls.py
+++ hostpital/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('paciente/', views.new_paciente, name='new_paciente'),
+    path('paciente/', views.paciente_list, name='paciente_list'),
+    path('agregar/paciente/', views.new_paciente, name='new_paciente'),
+    path('paciente/<pk>/edit/', views.paciente_edit, name='paciente_edit'),
+    path('paciente/<pk>/remove/', views.paciente_remove, name='paciente_remove'),
+
 ]
--- hostpital/views.py
+++ hostpital/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Pacientes
 from .forms import PacientesForm
+
+def paciente_list(request):
+    paciente = Pacientes.objects.all()
+    return render(request, 'hostpital/paciente_list.html',{'pacientes':paciente})
 
 def new_paciente(request):
     if request.method == "POST":
@@ -7,6 +12,24 @@ def new_paciente(request):
         if form.is_valid():
             Paciente = form.save(commit=False)
             Paciente.save()
+            return redirect('paciente_list')
     else:
         form= PacientesForm()
     return render(request, 'hostpital/new_paciente.html',{'form': form})
+
+def paciente_edit(request, pk):
+    pa= get_object_or_404(Pacientes, pk=pk)
+    if request.method== "POST":
+        form = PacientesForm(request.POST, instance=pa)
+        if form.is_valid():
+            pa = form.save(commit=False)
+            pa.save()
+            return redirect('paciente_list')
+    else:
+        form= PacientesForm(instance=pa)
+    return render(request, 'hostpital/paciente_edit.html',{'form': form})
+
+def paciente_remove(request, pk):
+    paciente= get_object_or_404(Pacientes, pk=pk)
+    paciente.delete()
+    return redirect('paciente_list')
