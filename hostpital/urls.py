9bb224e3657e9572eb25577cfdce46d4666c65bb
fernando velasquez <fvelasquez650@gmail.com>
2021-11-28 20:48:27 -0600

    added list,edit and delete functions

--- hostpital/urls.py
+++ hostpital/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('anestesia/', views.new_anestesia, name='new_anestesia'),
+    path('anestesia/', views.anestesia_list, name='anestesia_list'),
+    path('agregar/anestesia/', views.new_anestesia, name='new_anestesia'),
+    path('anestesia/<pk>/edit/', views.anestesia_edit, name='anestesia_edit'),
+    path('anestesia/<pk>/remove/', views.anestesia_remove, name='anestesia_remove'),
 ]
--- hostpital/views.py
+++ hostpital/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-from hostpital.models import Anestesia
+from .models import Anestesia
 from .forms import AnestesiaForm
+
+def anestesia_list(request):
+    anestesia = Anestesia.objects.all()
+    return render(request, 'hostpital/anestesia_list.html', {'anestesia': anestesia})
 
 def new_anestesia(request):
     if request.method == "POST":
@@ -9,6 +13,25 @@ def new_anestesia(request):
         if form.is_valid():
             Anestesia = form.save(commit=False)
             Anestesia.save()
+            return redirect('anestesia_list')
     else:
         form= AnestesiaForm()
     return render(request, 'hostpital/new_anestesia.html',{'form': form})
+
+
+def anestesia_edit(request, pk):
+    mant = get_object_or_404(Anestesia, pk=pk)
+    if request.method == "POST":
+        form = AnestesiaForm(request.POST, instance=mant)
+        if form.is_valid():
+            mant = form.save(commit=False)
+            mant.save()
+            return redirect('anestesia_list')
+    else:
+        form = AnestesiaForm(instance=mant)
+    return render(request, 'hostpital/anestesia_edit.html', {'form': form})
+
+def anestesia_remove(request, pk):
+    anestesia = get_object_or_404(Anestesia, pk=pk)
+    anestesia.delete()
+    return redirect('anestesia_list')
