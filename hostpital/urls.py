3ba0f378d4b9ad21a905a9282423b881765a37cc
Pelt06 <peltzjr11@gmail.com>
2021-11-29 00:13:52 -0600

    CRUD ASSISTANTS

--- hostpital/urls.py
+++ hostpital/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('Assistant/', views.new_Assistant, name='new_Assistant'),
+    path('Assistant/', views.list_Assistant, name='list_Assistant'),
+    path('agregar/Assistant', views.new_Assistant, name='new_Assistant'),
+    path('Assistant/<pk>/edit/', views.edit_Assistant, name='edit_Assistant'),
+    path('Assistant/<pk>/remove/', views.remove_Assistant, name='remove_Assistant'),
 ]
--- hostpital/views.py
+++ hostpital/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
 from hostpital.models import Assistant
 from .forms import AssistantForm
+
+def list_Assistant(request):
+    assist = Assistant.objects.all()
+    return render(request, 'hostpital/list_Assistant.html', {'Enfermeria': assist})
 
 def new_Assistant(request):
     if request.method == "POST":
         form = AssistantForm(request.POST)
         if form.is_valid():
-            Assistant = form.save(commit=False)
-            Assistant.save()
+            assist= form.save(commit=False)
+            assist.save()
     else:
         form= AssistantForm()
     return render(request, 'hostpital/new_Assistant.html',{'form': form})
+
+def edit_Assistant(request, pk):
+    assist = get_object_or_404(Assistant, pk=pk)
+    if request.method == "POST":
+        form = AssistantForm(request.POST, instance=assist)
+        if form.is_valid():
+            assist = form.save(commit=False)
+            assist.save()
+            return redirect('list_Assistant')
+    else:
+        form = AssistantForm(instance=assist)
+    return render(request, 'hostpital/edit_Assistant.html', {'form': form})
+
+def remove_Assistant(request, pk):
+    assist = get_object_or_404(Assistant, pk=pk)
+    assist.delete()
+    return redirect('list_Assistant')
